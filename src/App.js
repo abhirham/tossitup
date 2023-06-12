031bde564b561f3eb0ef341e9bc73fe73ae71054
abhirham <savarav.a@gmail.com>
2023-06-12 16:31:31 -0400

    added menu heading at top

--- src/App.js
+++ src/App.js
@@ -1,45 +1,57 @@
-import './App.css';
+import "./App.css";
 import { useState } from "react";
-import { food } from './constants';
+import { food } from "./constants";
 
-import CategoryTitle from './components/CategoryTitle';
-import ImagePicker from './components/ImagePicker';
+import CategoryTitle from "./components/CategoryTitle";
+import ImagePicker from "./components/ImagePicker";
 
 function App() {
-    const [activeCategory, setActiveCategory] = useState("appetizer");
-    let { title, foods } = food[activeCategory];
-    
-    return (
-        <div className='mainContent min-vh-100'>
-            <header className='logoHeader d-flex align-items-center justify-content-center'>
-                <img className='img-fluid logoText' src='/images/logoText.png'></img>
-            </header>
-            <main className="container my-5">
-                <ImagePicker onImageClick={setActiveCategory} activeCategory={activeCategory} />
-                <CategoryTitle category={title} />
-                <div className='row row-cols-1 row-cols-md-2 g-5'>
-                    {
-                        foods.map((x, idx) => (
-                            <div className='col' key={idx}>
-                                <div className='d-flex flex-row justify-content-between fw-bold'>
-                                    <span>{x.name}</span>
-                                    <span>${x.price}</span>
-                                </div>
-                                <div>{x.description}</div>
-                                {x.spice && <div className='d-flex flex-row justify-content-end mt-2'>{x.spice.map(s => <span className={s + " spicePill"} key={idx+s}>{s}</span>)}</div>}
-                            </div>
-                        ))
-                    }
-                </div>
-            </main>
-            <footer className='d-flex flex-column justify-content-center align-items-center pb-3'>
-                <div className='brand'>TOSS IT UP</div>
-                <div>1233 Dillon Rd, Burlington</div>
-                <div>ON L7M 1K6</div>
-                <div className='mt-2'>Phone - 905 497 5677</div>
-            </footer>
+  const [activeCategory, setActiveCategory] = useState("appetizer");
+  let { title, foods } = food[activeCategory];
+
+  return (
+    <div className="mainContent min-vh-100">
+      <header className="logoHeader d-flex align-items-center justify-content-center">
+        <img className="img-fluid logoText" src="/images/logoText.png"></img>
+      </header>
+      <main className="container mb-5">
+        <div className="text-center">
+          <span className="menu">MENU</span>
         </div>
-    );
+        <ImagePicker
+          onImageClick={setActiveCategory}
+          activeCategory={activeCategory}
+        />
+        <CategoryTitle category={title} />
+        <div className="row row-cols-1 row-cols-md-2 g-5">
+          {foods.map((x, idx) => (
+            <div className="col" key={idx}>
+              <div className="d-flex flex-row justify-content-between fw-bold">
+                <span>{x.name}</span>
+                <span>${x.price}</span>
+              </div>
+              <div>{x.description}</div>
+              {x.spice && (
+                <div className="d-flex flex-row justify-content-end mt-2">
+                  {x.spice.map((s) => (
+                    <span className={s + " spicePill"} key={idx + s}>
+                      {s}
+                    </span>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </main>
+      <footer className="d-flex flex-column justify-content-center align-items-center pb-3">
+        <div className="brand">TOSS IT UP</div>
+        <div>1233 Dillon Rd, Burlington</div>
+        <div>ON L7M 1K6</div>
+        <div className="mt-2">Phone - 905 497 5677</div>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ImagePicker.js
+++ src/components/ImagePicker.js
@@ -3,45 +3,63 @@ import { foodImageMap, foodImageList } from "../constants";
 import "./ImagePicker.css";
 
 export default ({ activeCategory, onImageClick }) => {
-    let current = foodImageMap[activeCategory];
+  let current = foodImageMap[activeCategory];
 
-    useEffect(() => {
-        document.querySelector('.'+activeCategory).scrollIntoView({inline: "center", behavior: "smooth", block: 'nearest'});
-        document.querySelectorAll('.foodImage').forEach(el => el.classList.remove("active"));
-        document.querySelector('.'+activeCategory).classList.add("active");
-    }, [activeCategory])
+  useEffect(() => {
+    document.querySelector("." + activeCategory).scrollIntoView({
+      inline: "center",
+      behavior: "smooth",
+      block: "nearest",
+    });
+    document
+      .querySelectorAll(".foodImage")
+      .forEach((el) => el.classList.remove("active"));
+    document.querySelector("." + activeCategory).classList.add("active");
+  }, [activeCategory]);
 
-    useEffect(() => {
-        const disableScroll = (e) => {
-            if (e.deltaX !== 0) {
-                e.preventDefault();
-            }
-        };
-    
-        const scrollDiv = document.querySelector('.foodImageContainer');
-    
-        scrollDiv.addEventListener('wheel', disableScroll, { passive: false });
-        scrollDiv.addEventListener('touchmove', disableScroll, { passive: false });
-    
-        return () => {
-          scrollDiv.removeEventListener('wheel', disableScroll);
-          scrollDiv.removeEventListener('touchmove', disableScroll);
-          
-        };
-      }, []);
+  useEffect(() => {
+    const disableScroll = (e) => {
+      if (e.deltaX !== 0) {
+        e.preventDefault();
+      }
+    };
 
+    const scrollDiv = document.querySelector(".foodImageContainer");
 
-    return (
-        <div className="d-flex flex-row justify-content-center align-items-center mt-5 mb-5 imagePicker">
-            <button onClick={e => onImageClick(current.prev.key)} className="iconBtn me-3 ">
-                <i className="fa-solid fa-angle-left"></i>
-            </button>
-            <div className="d-flex flex-row foodImageContainer">
-                {foodImageList.map(({key, image}, idx) => <img key={key+idx} src={`/images/${image}`} className={`img-fluid foodImage mx-3 ${key.indexOf("_cloned") > -1 ? key : key+" centered"}`}></img>)}
-            </div>
-            <button onClick={e => onImageClick(current.next.key)} className="iconBtn ms-2  ">
-                <i className="fa-solid fa-angle-right"></i>
-            </button>
-        </div>
-    )
-}
+    scrollDiv.addEventListener("wheel", disableScroll, { passive: false });
+    scrollDiv.addEventListener("touchmove", disableScroll, { passive: false });
+
+    return () => {
+      scrollDiv.removeEventListener("wheel", disableScroll);
+      scrollDiv.removeEventListener("touchmove", disableScroll);
+    };
+  }, []);
+
+  return (
+    <div className="d-flex flex-row justify-content-center align-items-center my-4 imagePicker">
+      <button
+        onClick={(e) => onImageClick(current.prev.key)}
+        className="iconBtn me-3 "
+      >
+        <i className="fa-solid fa-angle-left"></i>
+      </button>
+      <div className="d-flex flex-row foodImageContainer">
+        {foodImageList.map(({ key, image }, idx) => (
+          <img
+            key={key + idx}
+            src={`/images/${image}`}
+            className={`img-fluid foodImage mx-3 ${
+              key.indexOf("_cloned") > -1 ? key : key + " centered"
+            }`}
+          ></img>
+        ))}
+      </div>
+      <button
+        onClick={(e) => onImageClick(current.next.key)}
+        className="iconBtn ms-2  "
+      >
+        <i className="fa-solid fa-angle-right"></i>
+      </button>
+    </div>
+  );
+};
