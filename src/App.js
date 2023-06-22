5d539783ad4b70767f0af2e2301fd384ddc0eda5
“abhirham94” <abhirham@skynetsoftware.ca>
2023-06-22 15:31:10 -0400

    made final changes

--- src/App.js
+++ src/App.js
@@ -10,14 +10,32 @@ function App() {
   const [activeCategory, setActiveCategory] = useState("appetizer");
   let { title, foods } = food[activeCategory];
 
+  function viewMenu() {
+    document.querySelector(".menu").scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+
   return (
     <div className="mainContent min-vh-100">
-      <header className="logoHeader d-flex align-items-center justify-content-center">
+      <header className="logoHeader d-flex flex-column align-items-center justify-content-center">
         <img className="img-fluid logoText" alt="company_logo" src="/images/logoText.png"></img>
+        <button type="button" class="btn btn-warning viewMenuBtn mt-5 text-bangers fs-1 px-4" onClick={viewMenu}>View Menu</button>
       </header>
+      <div className="row aboutus align-items-center p-5">
+        <div className="col-md p-5">
+          <img className="img-fluid" alt="boryani_img" src="/images/about_us_biryani.jpeg"></img>
+        </div>
+        <div className="col-md p-5">
+          <div className="header">ABOUT<br></br>toss it up</div>
+          <p>We are an authentic Indian restaurant dedicated to serving a delightful array of culinary delights. From sizzling Manchuria and perfectly seasoned Wings to aromatic Biryani and refreshing salads, our menu captures the true essence of Indian cuisine.</p>
+          <p>Indulge in our flavorful dishes, crafted with care and infused with traditional spices. Complete your meal with a sweet treat like our delectable Gulab Jamun and quench your thirst with refreshing juices and smoothies.</p>
+          <p>Join us at TOSSITUP for a memorable dining experience that will transport you to the vibrant streets of India. Taste the tradition, savor the experience.</p>
+        </div>
+      </div>
       <main className="container mb-5">
         <div className="text-center">
-          <span className="menu">MENU</span>
+          <span className="menu text-bangers">MENU</span>
         </div>
         <ImagePicker
           onImageClick={setActiveCategory}
